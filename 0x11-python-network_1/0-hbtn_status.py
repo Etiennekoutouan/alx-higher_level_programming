373410bc31747af341a69a6dd0eb83f0364d16c6
koutouan etienne <etiennekoutouan53@gmail.com>
2023-07-10 13:35:11 -0700

    watch

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""A script that
-fetches https://intranet.hbtn.io/status.
+"""A script that fetches https://intranet.hbtn.io/status.
 """
 if __name__ == '__main__':
     import urllib.request
